cb8daea93a1ee75500c60e0bed58a93456c1cc42
Geobellz22 <st377126@gmail.com>
2023-10-05 22:23:54 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotated function floor which takes a float
+n as an argument abd returns the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """Returns floor of the float"""
+    return int(n)
