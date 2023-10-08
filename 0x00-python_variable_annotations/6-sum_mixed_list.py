9bf70882835b333864001dc1dc4d9e61748581c8
Geobellz22 <st377126@gmail.com>
2023-10-08 10:33:30 +0000

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Type annotated function sum_mixed_list which takes
+a list mxd_list of integers and float
+abd returns their sum as float"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Returns sum of float"""
+    return sum(mxd_list)
