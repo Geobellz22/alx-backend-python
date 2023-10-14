da87157d03a1e8694d04f586dd92ddb65206772a
Geobellz22 <st377126@gmail.com>
2023-10-14 21:18:28 +0000

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Async routine called task_wait_n that takes in 2 int
+arguments (in this order): n and max_delay
+"""
+import asyncio
+from random import uniform
+from typing import List
+
+
+async def task_wait_random(max_delay: int) -> float:
+    """
+    Generate a random delay between 0 and max_delay.
+
+    Args:
+        max_delay (int): The maximum delay allowed.
+
+    Returns:
+        float: A random delay value.
+
+    """
+    return uniform(0, max_delay)
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn task_wait_random n times to generate random delays.
+
+    Args:
+        n (int): The number of times to generate random delays.
+        max_delay (int): The maximum delay allowed.
+
+    Returns:
+        List[float]: A sorted list of random delays.
+
+    """
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*delays)
+    return sorted(results)
