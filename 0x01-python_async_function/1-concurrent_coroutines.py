8f58b633486174c02424beb4302a328f128945a4
Geobellz22 <st377126@gmail.com>
2023-10-14 21:01:49 +0000

    Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,13 +7,31 @@ import asyncio
 from random import uniform
 from typing import List
 
-
 async def wait_random(max_delay: int) -> float:
+    """
+    Generate a random delay between 0 and max_delay.
+
+    Args:
+        max_delay (int): The maximum delay allowed.
+
+    Returns:
+        float: A random delay value.
+
+    """
     return uniform(0, max_delay)
 
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times"""
+    """
+    Spawn wait_random n times to generate random delays.
+
+    Args:
+        n (int): The number of times to generate random delays.
+        max_delay (int): The maximum delay allowed.
+
+    Returns:
+        List[float]: A sorted list of random delays.
+
+    """
     delays = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*delays)
     return sorted(results)
